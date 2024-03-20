c2ab92fcd766d4deb63b7c0ae0dc673da45d63c6
Camilla <comincioli.006240@laba.edu>
2024-03-20 10:29:05 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import Works from './components/Works.vue';
+</script>
+
+<template>
+  <main>
+    <Works />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 0rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
